f20e3065484540d43dd6ebefb1331feb5abab95d
SandyWKX <kwu3@wpi.edu>
2018-07-13 14:39:28 -0400

    Delete redundant info

--- src/Leetcode/array/TransposeMatrix.java
+++ src/Leetcode/array/TransposeMatrix.java
@@ -2,17 +2,11 @@ package Leetcode.array;
 //No.867
 public class TransposeMatrix {
     public static void main(String[] args){
-    int[][] A = {{1,2,3},{4,5,6}};
-    TransposeMatrix matrix = new TransposeMatrix();
-    int[][] B = matrix.transpose(A);
-  /*for (int i =0; i<B.length;i++){
-        for (int j = 0; j<B[i].length;j++){
-            System.out.println(B[i][j]);
-            //System.out.println(B.length);
-            //System.out.println("A: "+A[i][j]);
-            }
-        }*/
+        int[][] A = {{1,2,3},{4,5,6}};
+        TransposeMatrix matrix = new TransposeMatrix();
+        int[][] B = matrix.transpose(A);
     }
+
     int[][] transpose(int[][] A){
         int row = A.length;
         int col = A[0].length;
